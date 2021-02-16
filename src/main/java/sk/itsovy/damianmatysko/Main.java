bfa317ee05887bc173503ce18b0db3c75f333478
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-16 22:56:40 +0100

    Update Main.java
    changed

--- src/main/java/sk/itsovy/damianmatysko/Main.java
+++ src/main/java/sk/itsovy/damianmatysko/Main.java
@@ -3,7 +3,8 @@ package sk.itsovy.damianmatysko;
 public class Main {
     public static void main(String[] args) {
         NumberIdentificator obj = new NumberIdentificator();
-        NumberInterface ref = () -> obj.isEvenNumber(10);
-        System.out.println(ref.resolve());
+        NumberInterface refer = () -> obj.isEvenNumber(10);
+        // comment
+        System.out.println(refer.resolve());
     }
 }
