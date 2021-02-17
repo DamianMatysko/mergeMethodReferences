fd84bf3021c354b58e817a501993d01ecccc826d
DamianMatysko <mada11@azet.sk>
2021-02-17 19:36:01 +0100

    add

--- src/main/java/sk/itsovy/damianmatysko/NumberIdentificator.java
+++ src/main/java/sk/itsovy/damianmatysko/NumberIdentificator.java
@@ -7,4 +7,5 @@ public class NumberIdentificator {
         }
         return false;
     }
+
 }
--- src/main/java/sk/itsovy/damianmatysko/NumberInterface.java
+++ src/main/java/sk/itsovy/damianmatysko/NumberInterface.java
@@ -3,4 +3,5 @@ package sk.itsovy.damianmatysko;
 @FunctionalInterface
 interface NumberInterface {
     boolean resolve();
+
 }
